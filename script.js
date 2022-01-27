a41c72257eec433a804b96e0daf51417d91d86c0
Victor Volkov <vityan2504@gmail.com>
2022-01-27 10:49:43 +0300

    создал переменные и вывел сообщения

---
+++ script.js
@@ -0,0 +1,16 @@
+////////////////////
+
+let title,
+screens,
+screenPrice,
+rollback,
+fullPrice,
+adaptive;
+
+////////////////////
+
+alert("hello World");
+
+////////////////////
+
+console.log('Hi world');
