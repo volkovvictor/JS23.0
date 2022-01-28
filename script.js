71056e037a5c873d87847b78b0a30a5e3e6f32f0
Victor Volkov <vityan2504@gmail.com>
2022-01-28 11:15:11 +0300

    feat (script.js) произвёл некоторые операции над переменными и вывел в консоль

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 ////////////////////
 
-let title,
-screens,
-screenPrice,
-rollback,
-fullPrice,
-adaptive;
+let title = 'Какой-то проект',
+screens = "Простой, Сложный, Интерактивный",
+screenPrice = 3000,
+rollback = 20,
+fullPrice = 60000,
+adaptive = true;
 
 ////////////////////
 
-alert("hello World");
 
-////////////////////
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log('Hi world');
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.toLocaleLowerCase().split(', '));
+
+console.log(`Процент отката посреднику за работу ${fullPrice * (rollback/100)}`);
